575a91386eb38054da775a33fbe6f35392e1c7b6
lokesh <revanurulokesh@gmail.com>
2024-03-02 00:43:02 +0530

    finding cars by id

---
+++ problem1.js
@@ -0,0 +1,16 @@
+
+function findCarByID(arr,id)
+{
+    if(Array.isArray(arr)&&id)
+    {
+    for(let car of arr){
+        if (car.id===id){
+            return `car ${id} is a ${car.car_year} ${car.car_make} ${car.car_model}`
+        }
+    }
+}
+};
+
+module.exports=findCarByID;
+
+//console.log(carDetails(inventory,33));
